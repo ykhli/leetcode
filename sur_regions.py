5b73f8d09a5a50a5811807927c22aff6c3852991
Yoko Hongyu Li <yokolee1013@gmail.com>
2014-07-07 00:45:36 -0500

    surrounded region problem done

--- sur_regions.py
+++ sur_regions.py
@@ -8,15 +8,15 @@ class Solution:
         # go through the outmost layer
         rowNum = len(board)
         colNum = len(board[0])
-        visited = [[False for i in range(0,colNum)] for j in range(0,rowNum)]
+        visited = [[False for i in xrange(0,colNum)] for j in xrange(0,rowNum)]
         q = []
-        for row in range(0,len(board)):
+        for row in xrange(0,len(board)):
             if board[row][0] == 'O':
                 q.append((row,0))
             if board[row][len(board[0])-1] == 'O':
                 q.append((row,len(board[0])-1))
 
-        for col in range(1,len(board[0])):
+        for col in xrange(1,len(board[0])):
             if board[0][col] == 'O':
                 q.append((0,col))
             if board[len(board)-1][col] == 'O':
@@ -24,37 +24,22 @@ class Solution:
 
         while q:
             v = q.pop(0)
-            nbs = Solution.neighbor(board,v[0],v[1])
+            q += Solution.neighbor(visited,board,v[0],v[1])
             # change all reachable 'O's to '$'
-            Solution.change(board,v[0],v[1],'$')
-            for n in nbs:
-                if visited[n[0]][n[1]] == False:
-                    q.append(n)
+            if board[v[0]][v[1]] == 'O':
+                board[v[0]][v[1]] = '$'
             visited[v[0]][v[1]] = True
 
-        for row in range(0,len(board)):
-            for col in range(0,len(board[0])):
+        for row in xrange(0,len(board)):
+            for col in xrange(0,len(board[0])):
                 if board[row][col] == '$':
-                    Solution.change(board,row,col,'O')
+                    board[row][col] = 'O'
                 elif board[row][col] == 'O':
-                    Solution.change(board,row,col,'X')
+                    board[row][col] = 'X'
         return board
 
     @staticmethod
     def neighbor(board,row,col):
         neighbors = [(row-1,col),(row+1,col),(row,col-1),(row,col+1)]
-        return filter(lambda (x,y): x>=0 and x<len(board) and y>=0 and y<len(board[0]) and board[x][y] == 'O', neighbors) 
+        return filter(lambda (x,y): x>=0 and x<len(board) and y>=0 and y<len(board[0]) and board[x][y] == 'O' and visited[row][col] == False, neighbors) 
 
-    @staticmethod
-    def change(board,row,col,val):
-        tmp = list(board[row])
-        tmp[col] = val
-        board[row] = ''.join(tmp)
-    
-if __name__ == "__main__":
-    x = Solution()
-    #board = [['X','X','X','X'],['X','O','O','X'],['X','X','O','X'],['X','O','X','X']]
-    #board = [['O','X','X','O','X'],['X','O','O','X','O'],['X','O','X','O','X'],['O','X','O','O','O'],['X','X','O','X','O']]
-    board = ["XOXX","OXOX","XOXO","OXOX","XOXO","OXOX"]
-    #board = ["OOOOOOOO","OOOOXOOO","OXOOOOOO","OOOOOOOO","XOOXOOXO","OOOOOOOO","OOOOOOOO","OOOOOOOO"]
-    print x.solve(board)
